23360a14dd5226ea78cf3fe18e5c7ddfb680a6ea
Sysinfocus <Sysinfocus@users.noreply.github.com>
2025-02-28 04:41:07 +0000

    Deploying to gh-pages from  @ fd8000716a5f2d06025e04d4180cad01c31390a6 🚀

---
+++ _content/Sysinfocus.AspNetCore.Components/enable-push-notifications.js
@@ -0,0 +1,27 @@
+﻿function init() {
+    if ("Notification" in window) {
+        if (Notification.permission === "default") {
+            Notification.requestPermission().then(permission => {
+                if (permission === "granted")  return true;
+                else return false;
+            });
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function sendNotification(title, body, icon = null, url = null, tag = null) {
+    const options = { body: body, icon: icon, url: url, tag: tag };
+    if (Notification.permission === "granted") {
+        const notification = new Notification(title, options);
+        if (url) {
+            notification.onclick = function () { window.open(url, '_blank'); notification.close(); };
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
